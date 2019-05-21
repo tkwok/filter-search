9425915f0c8815440d3b58a801d893acafb67fea
Tony Kwok <holypaladin07@gmail.com>
2019-05-21 00:51:44 -0700

    start moving template search/filter into code

--- main.js
+++ main.js
@@ -16,11 +16,31 @@ const Main = {
   },
   Create: function() {
     const template = `
-      
+      <div class="input-icon-magnifier">
+        <input id="input-search" class="select-partner" type="text" placeholder="Search"/>
+        <img src=""/>
+      </div>
+      <label for="select-country" class="label-common">Country</label>
+      <div class="select-icon-arrow">
+        <select id="select-country" class="select-partner">
+          <option value="-1">All countries</option>
+        </select>
+      </div>
+      <label for="select-region" class="label-common">Region</label>
+      <div class="select-icon-arrow">
+        <select id="select-region" class="select-partner">
+          <option value="-1">All regions</option>
+        </select>
+      </div>
+      <label for="select-tier-level" class="label-common">Tier Level</label>
+      <div class="select-icon-arrow">
+        <select id="select-tier-level" class="select-partner">
+          <option value="-1">All levels</option>
+        </select>
+      </div>
+      <a href="" id="link-clear-all-filters"><strong>Clear all filters</strong></a>
     `;
-    event.innerHTML = html.join('');
-    Pagination.event = event.getElementsByTagName('span')[0];
-    Pagination.parentElement = event;
+    event.innerHTML = template;
   },
   Init: function() {
     Main.Extend();
